5c44bbaf89793cca6c0e8e8ac6de02da99f08b88
tbtristan <>
2021-04-17 22:19:05 +0000

    push it real good

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 # Tools & APIS  – Python (Coding)
 #                 Cloud Natural Language API 
 #                 Dialogflow API
+#                 Text-to-Speech API
 #                 echoAR
 #
 # Members       – Tristan Bradfield
@@ -20,5 +21,28 @@
 #					 general comments based on sentiment over past N tweets
 #					 make it work in AR, have it run around doin gay little memes
 
-from google.cloud import dialogflow
+#from google.cloud import dialogflow
 from google.cloud import language
+#from google.cloud import texttospeech
+
+def language_analysis(text):
+  client  = language.Client()
+
+  document  = client.document_from_text(text)
+
+  sent_analysis = document.analyze_sentiment()
+  print(dir(sent_analysis))
+
+  sentiment = sent_analysis.sentiment
+  print(sentiment)
+
+  ent_analysis  = document.analyze_entities()
+  entities      = ent_analysis.entities
+  for e in entities:
+    print(e.name, e.entity_type, e.metadata, e.salience)
+
+
+if __name__ == '__main__':
+  text = "The circumplex model on the other hand, needs these data points to complete the “circle” that gives the model its name. Rubin & Talarico (2009) had participants rate emotion related stimuli on scales for valence and intensity (or arousal), and attempted to fit the circumplex and vector models on this data. Interestingly, they were unable to find high-intensity neutral words, which gives credit to the vector model over the circumplex model when dealing with textual data."
+
+  language_analysis(text)
