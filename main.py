b0434ce76b941c6a7291ec7826937f05961de0cc
tbtristan <>
2021-04-17 20:54:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#################
+# Project Title – BitBerg Bot
+
+# Description   – Twitter bot that simulates conversation 
+#                 to your tweets
+
+# Tools & APIS  – Python (Coding)
+#                 Cloud Natural Language API 
+#                 Dialogflow API
+#                 echoAR
+#
+# Members       – Tristan Bradfield
+#                 Aidan Gray
+#                 Kameron Melvin
+#################
+
+#modules : sentiment analysis on users tweets
+#					 gifs and sprites
+#					 chat responses based on sentiment
+#					 general comments based on sentiment over past N tweets
+#					 make it work in AR, have it run around doin gay little memes
+
+from google.cloud import dialogflow
+from google.cloud import language
